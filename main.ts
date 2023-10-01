e9d2fabe8fd9edfc93dc8b911be94974652db829
alexbraadworst <96457501+alexbraadworst@users.noreply.github.com>
2023-09-30 21:34:31 -0500

    Added better scoring

--- main.ts
+++ main.ts
@@ -14,7 +14,6 @@ radio.onReceivedString(function (receivedString) {
             basic.pause(1000)
             basic.clearScreen()
             radio.sendValue("ready", 1)
-            freqIsSet = true
             gameReady = true
             break;
         } else if (input.buttonIsPressed(Button.A)) {
@@ -33,40 +32,75 @@ radio.onReceivedValue(function (name, value) {
         hand1 = value
     } else if (name == "hand2str") {
         hand2 = value
+    } else if (name == "score1str") {
+        score2 = value
     }
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     isPlayer = true
 })
+let waiting = false
 let isPlayer = false
 let hand2 = 0
 let hand1 = 0
 let score2 = 0
 let score1 = 0
 let gameReady = false
-let freqIsSet = false
 let frequency = 0
 basic.pause(100)
 radio.sendString("pairing")
 basic.forever(function () {
+    waiting = false
+    score1 = 0
+    score2 = 0
     while (gameReady == true) {
         if (isPlayer == true) {
-            if (hand1 == 1) {
-                basic.showIcon(IconNames.SmallSquare)
-            } else if (hand1 == 2) {
-                basic.showIcon(IconNames.Square)
-            } else if (hand1 == 3) {
-                basic.showIcon(IconNames.Scissors)
-            } else {
-                basic.showIcon(IconNames.No)
-            }
             if (input.isGesture(Gesture.Shake)) {
                 hand1 = randint(1, 3)
                 hand2 = randint(1, 3)
                 radio.sendValue("hand1str", hand1)
                 radio.sendValue("hand2str", hand2)
             }
-            basic.showNumber(hand1)
+            if (hand1 == 1) {
+                basic.showIcon(IconNames.SmallSquare)
+                basic.pause(100)
+                basic.clearScreen()
+            } else if (hand1 == 2) {
+                basic.showIcon(IconNames.Square)
+                basic.pause(100)
+                basic.clearScreen()
+            } else if (hand1 == 3) {
+                basic.showIcon(IconNames.Scissors)
+                basic.pause(100)
+                basic.clearScreen()
+            } else {
+                basic.showIcon(IconNames.No)
+            }
+            if (waiting == false) {
+                if (hand1 == 1 && hand2 == 3 || (hand1 == 2 && hand2 == 1 || hand1 == 3 && hand2 == 2)) {
+                    score1 += 1
+                    basic.showIcon(IconNames.Happy)
+                    basic.pause(100)
+                    basic.clearScreen()
+                    basic.showNumber(score1)
+                    radio.sendValue("score1str", score1)
+                    waiting = true
+                } else if (hand1 == 1 && hand2 == 2 || (hand1 == 2 && hand2 == 3 || hand1 == 3 && hand2 == 1)) {
+                    basic.showIcon(IconNames.Sad)
+                    basic.pause(100)
+                    basic.clearScreen()
+                    basic.showNumber(score1)
+                    waiting = true
+                } else if (hand1 == 1 && hand2 == 1 || (hand1 == 2 && hand2 == 2 || hand1 == 3 && hand2 == 3)) {
+                    basic.showIcon(IconNames.No)
+                    basic.pause(100)
+                    basic.clearScreen()
+                    basic.showNumber(score1)
+                    waiting = true
+                }
+            } else {
+            	
+            }
         } else if (isPlayer == false) {
             if (hand2 == 1) {
                 basic.showIcon(IconNames.SmallSquare)
@@ -77,7 +111,31 @@ basic.forever(function () {
             } else {
                 basic.showIcon(IconNames.No)
             }
-            basic.showNumber(hand2)
+            if (waiting == false) {
+                if (hand1 == 1 && hand2 == 3 || (hand1 == 2 && hand2 == 1 || hand1 == 3 && hand2 == 2)) {
+                    basic.showIcon(IconNames.Sad)
+                    basic.pause(100)
+                    basic.clearScreen()
+                    basic.showNumber(score2)
+                    waiting = true
+                } else if (hand1 == 1 && hand2 == 2 || (hand1 == 2 && hand2 == 3 || hand1 == 3 && hand2 == 1)) {
+                    score2 += 1
+                    basic.showIcon(IconNames.Happy)
+                    basic.pause(100)
+                    basic.clearScreen()
+                    basic.showNumber(score2)
+                    radio.sendValue("score2str", score2)
+                    waiting = true
+                } else if (hand1 == 1 && hand2 == 1 || (hand1 == 2 && hand2 == 2 || hand1 == 3 && hand2 == 3)) {
+                    basic.showIcon(IconNames.No)
+                    basic.pause(100)
+                    basic.clearScreen()
+                    basic.showNumber(score2)
+                    waiting = true
+                }
+            } else {
+            	
+            }
         }
     }
 })
